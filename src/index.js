67d879ac4171a1e32cfcadaf72184cb2b6cec570
ajpignat <ajpignat@bigpond.net.au>
2022-03-06 19:34:07 +0800

    updated

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import Pages from './pages';
 import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000',
-  // uri: 'https://ajp-test-server.herokuapp.com/',
+  // uri: 'http://localhost:4000',
+  uri: 'https://ajp-test-server.herokuapp.com/',
   cache: new InMemoryCache(),
 });
 
